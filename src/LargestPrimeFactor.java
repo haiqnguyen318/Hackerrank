00c0c39470986dec533aa13eef4c482fe3d00c5d
Freddy Nguyen <haiqnguyen318@gmail.com>
2014-10-16 18:26:01 -0400

    Project Euler problem 2

--- src/LargestPrimeFactor.java
+++ src/LargestPrimeFactor.java
@@ -1,9 +1,38 @@
+import java.util.Scanner;
+
 
 public class LargestPrimeFactor {
-
+  private static boolean isPrime(long n) { 
+    if (n % 2 == 0) return false;
+    else {
+      long divisor = 3;
+      while (divisor <= Math.sqrt(n)) {
+        if (n % divisor == 0) return false;
+        divisor += 2;
+      }
+      return true;
+    }
+  }
+  
+  private static long largestPrimeFactor(long n) {
+    if (isPrime(n)) return n;
+    else {
+      if (n % 2 == 0) return largestPrimeFactor (n/2);
+      else {
+        long divisor = 3;
+        while (n % divisor != 0)
+          divisor += 2;
+        return largestPrimeFactor(n/divisor);
+      }
+    }
+  }
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    Scanner in = new Scanner(System.in);
+    int T = in.nextInt();
+    for (int t = 0; t < T; t++) {
+      long N = in.nextLong();
+      System.out.println(largestPrimeFactor(N));
+    }
   }
 
 }
